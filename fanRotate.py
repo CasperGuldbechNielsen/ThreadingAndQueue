43b8c22a20d0fd823f2b28fa58b2dc41769774d3
Casper Nielsen <Casp674d@edu.easj.dk>
2017-03-29 14:05:24 +0200

    Code refactoring

--- fanRotate.py
+++ fanRotate.py
@@ -20,8 +20,9 @@ class FanRotate(threading.Thread):
             print("Setup pins")
             GPIO.setup(pin, GPIO.OUT)
             GPIO.output(pin, False)
-
-        self.Seq = [[1, 0, 0, 1], [1, 0, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 0], [0, 0, 1, 1], [0, 0, 0, 1]]
+            self.Seq = []
+        for x in range(0, 8):
+            self.Seq.append([[1, 0, 0, 1], [1, 0, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0], [0, 1, 1, 0], [0, 0, 1, 0], [0, 0, 1, 1], [0, 0, 0, 1]])
 
         self.StepCount = len(self.Seq)
         self.StepDir = 0
@@ -40,27 +41,33 @@ class FanRotate(threading.Thread):
                 print("Queue {0} is empty..".format(self.q_name))
 
     def rotate(self, msg):
-        if (str(msg).__contains__("value='1'")):
-            self.StepDir = 1
-        elif (str(msg).__contains__("value='-1'")):
-            self.StepDir = -1
-        else:
-            self.StepDir = 0
 
-        for pin in range(0, 4):
-            xpin = self.StepPins[pin]
-            if self.Seq[self.StepCounter][pin] != 0:
-                GPIO.output(xpin, True)
+        rotating = True
+
+        while (rotating):
+            if (str(msg).__contains__("value='1'")):
+                self.StepDir = 1
+            elif (str(msg).__contains__("value='-1'")):
+                self.StepDir = -1
             else:
-                GPIO.output(xpin, False)
+                self.StepDir = 0
+            if (self.StepDir != 0):
+                for pin in range(0, 4):
+                    xpin = self.StepPins[pin]
+                    if self.Seq[self.StepCounter][pin] != 0:
+                        GPIO.output(xpin, True)
+                    else:
+                        GPIO.output(xpin, False)
 
-        self.StepCounter += self.StepDir
+                self.StepCounter += self.StepDir
 
-        if (self.StepCounter >= self.StepCount):
-            self.StepCounter = 0
-            self.StepDir = 0
-        if (self.StepCounter < 0):
-            self.StepCounter = self.StepCount + self.StepDir
-            self.StepDir = 0
+            if (self.StepCounter >= self.StepCount):
+                self.StepCounter = 0
+                self.StepDir = 0
+                rotating = False
+            if (self.StepCounter < 0):
+                self.StepCounter = self.StepCount + self.StepDir
+                self.StepDir = 0
+                rotating = False
 
         self.ready.set()
